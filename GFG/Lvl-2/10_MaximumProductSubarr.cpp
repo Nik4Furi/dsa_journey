63b5ce0c098384e725c22defc5705c6f74e0f9da
Nik4Furi <nikhilgurjar2212@gmail.com>
2023-12-14 09:07:43 +0530

    Day-5, doing array questions from GFG/leetcode

---
+++ GFG/Lvl-2/10_MaximumProductSubarr.cpp
@@ -0,0 +1,65 @@
+/*
+Given an array Arr[] that contains N integers (may be positive, negative or zero). Find the product of the maximum product subarray.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {6, -3, -10, 0, 2}
+Output: 180
+Explanation: Subarray with maximum product
+is [6, -3, -10] which gives product as 180.
+*/
+
+//Approach-I: 2 nested loops
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	// Function to find maximum product subarray
+	long long maxProduct(vector<int> arr, int n) {
+	    // code here
+	    
+	    long long prefix = 0,suffix=0,maxi=arr[0];
+	    
+	    for(int i=0;i<n;i++){
+	        if(prefix == 0) prefix = 1;
+	        if(suffix == 0) suffix = 1;
+	        
+	        prefix *= arr[i];
+	        suffix *= arr[n-i-1];
+	        
+	        maxi = max({prefix, suffix,maxi});
+	        
+	    }
+	    
+	    return maxi;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
